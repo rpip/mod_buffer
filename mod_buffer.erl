5b222cb43d073f501234e7eae4167657c7d02f68
Mawuli Adzaku <mawuli@mawuli.me>
2013-06-20 22:30:29 +0000

    Add twitter_pid, and buffer keys to the state record. Depend on mod_twiter for api_login and api_password configs

--- mod_buffer.erl
+++ mod_buffer.erl
@@ -31,7 +31,7 @@
 -define(SERVER, ?MODULE). 
 -include_lib("zotonic.hrl").
 -include_lib("modules/mod_admin/include/admin_menu.hrl").
--record(state, {context, buffer_queue}).
+-record(state, {context, buffer, buffer}).
 
 %% Request URLS
 %define(Twitter, https://api.twitter.com/1/statuses/update.json
@@ -80,9 +80,20 @@ init(Args) ->
    {context, Context} = proplists:lookup(context, Args),
    z_notifier:observe(restart_twitter, self(), Context),
 
-   %% load all buffers from db
-   BufferQueue = [],
-   {ok, #state{context=z_context:new(Context), buffer_queue=BufferQueue}}.
+   %% load all buffered posts from db
+   Buffer = [],
+
+   %% Start the twitter process
+   case share_buffer({Buffer,Context}) of
+        Pid when is_pid(Pid) ->
+            {ok, #state{context=z_context:new(Context),twitter_pid=Pid, buffer=Buffer}};
+        undefined ->
+            {ok, #state{context=z_context:new(Context)}};
+        not_configured ->
+            z_session_manager:broadcast(#broadcast{type="error", message="No configuration (mod_twitter.api_login / mod_twitter.api_password) found, not starting.", title="Social Buffer", stay=true}, z_acl:sudo(Context)),
+            {ok, #state{context=z_context:new(Context)}}
+    end.
+
 
 %%--------------------------------------------------------------------
 %% @private
@@ -112,6 +123,18 @@ handle_call(_Request, _From, State) ->
 %%                                  {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+handle_cast({restart_twitter, _Context}, #state{context=Context,twitter_pid=Pid,buffer=Buffer}=State) ->
+    case Pid of
+        undefined ->
+            %% not running
+            Pid2 = share_buffer({Buffer, Context}),
+            {noreply, #state{context=Context,twitter_pid=Pid2, buffer=Buffer}};
+        _ ->
+            %% Exit the process; will be started again.
+            erlang:exit(Pid, restarting),
+            {noreply, State#state{twitter_pid=undefined,buffer=Buffer}}
+    end;
+
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -139,7 +162,8 @@ handle_info(_Info, State) ->
 %% @spec terminate(Reason, State) -> void()
 %% @end
 %%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
+terminate(_Reason, State) ->
+    z_notifier:observe(restart_twitter, self(), State#state.context),
     ok.
 
 %%--------------------------------------------------------------------
@@ -156,3 +180,23 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+share_buffer({Buffer, Context}) when is_list(Buffer)->
+    Login = case m_config:get_value(?MODULE, api_login, false, Context) of
+                LB when is_binary(LB) ->
+                    binary_to_list(LB);
+                L -> L
+            end,
+    Pass  = case m_config:get_value(?MODULE, api_password, false, Context) of
+                LP when is_binary(LP) ->
+                    binary_to_list(LP);
+                P -> P
+            end,
+    lager:info("Twitter: (~p) Username = ~p", [z_context:site(Context), Login]),
+    case Login of
+        false ->
+            lager:warning("Twitter: (~p) No username/password configuration.", [z_context:site(Context)]),
+            not_configured;
+        _ ->
+            z_session_manager:broadcast(#broadcast{type="notice", message="ready to share social buffer...", title="Social Buffer", stay=false}, Context), 
+            %% spawn a share process and return the pid
+            spawn_link(?MODULE, share, [URL, Buffer, 5, Context])
