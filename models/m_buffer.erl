83971143d67b2089a7c69c97ff4b92168e2c91c7
Mawuli Adzaku <mawuli@mawuli.me>
2013-06-26 11:34:04 +0000

    Add update to m_buffer

--- models/m_buffer.erl
+++ models/m_buffer.erl
@@ -60,8 +60,7 @@ get(BufferId, Context) ->
 %% @spec insert(Content::string(), Tags::string(), Schedule::string(), Destination::string(), Status::int(), Context) -> {ok, BufferId} | {error, Reason}
 %% @todo Convert schedule date to postgresql compatible timestamp
 insert(Content, Tags, Schedule, Destination, Status, Context) ->
-    case z_auth:is_auth(Context) 
-            orelse z_convert:to_bool(m_config:get_value(mod_buffer, anonymous, true, Context)) of
+    case z_auth:is_auth(Context) of
         true ->
             %Schedule = z_convert:to_integer(Schedule),
 	    Props = [
@@ -70,7 +69,7 @@ insert(Content, Tags, Schedule, Destination, Status, Context) ->
                 {tags, z_html:escape(z_string:trim(Tags))},
 %                {schedule, Schedule},
                 {destination, z_string:trim(Destination)},
-                {gravatar_code, z_convert:to_integer(Status)},
+                {status, z_convert:to_integer(Status)},
                 {created, z_utils:now_msec()},
                 {modified, z_utils:now_msec()}
             ],
@@ -86,6 +85,35 @@ insert(Content, Tags, Schedule, Destination, Status, Context) ->
             {error, eacces}
     end.
 
+
+%% @doc Update a Buffer. Fetches the user information from the Context.
+%% @spec update(BufferId::integer(),Content::string(), Tags::string(), Schedule::string(), Destination::string(), Status::int(), Context) -> {ok, BufferId} | {error, Reason}
+%% @todo Convert schedule date to postgresql compatible timestamp
+update(BufferId, Content, Tags, Schedule, Destination, Status, Context) ->
+    case z_auth:is_auth(Context) of
+        true ->
+            %Schedule = z_convert:to_integer(Schedule),
+	    Props = [
+                {id, BufferId},
+                {content, z_html:escape(z_string:trim(Content))},
+                {tags, z_html:escape(z_string:trim(Tags))},
+%                {schedule, Schedule},
+                {destination, z_string:trim(Destination)},
+                {status, z_convert:to_integer(Status)},
+                {created, z_utils:now_msec()},
+                {modified, z_utils:now_msec()}
+            ],
+            case z_db:update(buffer, BufferId, Props, Context) of
+                {ok, BufferId} = Result ->
+                    z_depcache:flush({buffer, BufferId}, Context),
+                    z_notifier:notify({buffer,BufferId}, Context),
+                    Result;
+                {error, _} = Error ->
+                    Error
+            end;
+        false ->
+            {error, eacces}
+    end.
 
 %% @doc Delete a buffer.
 delete(BufferId, Context) ->
